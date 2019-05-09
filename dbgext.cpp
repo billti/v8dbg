9f6c4dab418dd84268b30596f1e1cc12e9859ca1
Bill Ticehurst <billti@microsoft.com>
2019-05-09 14:08:01 -0700

    Initial commit

---
+++ dbgext.cpp
@@ -0,0 +1,14 @@
+﻿// dbgext.cpp : Defines the entry point for the application.
+//
+
+#include "dbgext.h"
+
+using namespace std;
+
+__declspec(dllexport) int mymain()
+{
+	void* pDebugClient;
+	DebugCreate(__uuidof(IDebugClient), (void**)&pDebugClient);
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ dbgext.h
@@ -0,0 +1,9 @@
+﻿// dbgext.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <DbgEng.h>
+
+// TODO: Reference additional headers your program requires here.
